4f6afc43ed97807468754d123eef628d04cdf581
Ruby101-was-taken <abusinessemailkinda@gmail.com>
2024-11-13 15:00:52 +0000

    :confetti_ball: THE BEGINNING!!!!!!!
    - Added the goods :trollface:

---
+++ CA2 - Ruby White - GD3B/CA2 - Ruby White - GD3B.cpp
@@ -0,0 +1,10 @@
+// CA2 - Ruby White - GD3B.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "BinaryTree.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
